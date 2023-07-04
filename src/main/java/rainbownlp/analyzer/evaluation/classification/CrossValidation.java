4d1e4d00d1eaea3b9b5b9349c3483020832f3bce
ExpressLens <expresslens@proton.me>
2023-07-04 21:31:53 +0000

    Removed obsolete tests

--- src/main/java/rainbownlp/analyzer/evaluation/classification/CrossValidation.java
+++ src/main/java/rainbownlp/analyzer/evaluation/classification/CrossValidation.java
@@ -11,4 +11,39 @@ import rainbownlp.util.FileUtil;
 import rainbownlp.util.ConfigurationUtil;
 
 
-public class CrossVa
+public class CrossValidation implements ICrossfoldValidator {
+	
+	LearnerEngine mlModel;
+	public CrossValidation(LearnerEngine learningEngine)
+	{
+		mlModel = learningEngine;
+	}
+	public EvaluationResult crossValidation(List<MLExample> examples, int folds) throws Exception
+	{
+
+		int foldCount = examples.size()/folds;
+		ArrayList<EvaluationResult> results = 
+			new ArrayList<EvaluationResult>();
+		for(int foldIndex = 0;foldIndex<folds;foldIndex++)
+		{
+			ConfigurationUtil.crossFoldCurrent = foldIndex+1;
+			int start_index = foldIndex*foldCount;
+			int end_index = (foldIndex+1)*foldCount;
+			if(end_index>=examples.size()) 
+				end_index = examples.size();
+			
+//			HibernateUtil.startTransaction();
+			
+			List<MLExample> train_set = new ArrayList<MLExample>();
+			for(int i=0;i<start_index;i++)
+				train_set.add(examples.get(i).clone());
+			for(int i=end_index;i<examples.size();i++)
+				train_set.add(examples.get(i).clone());
+				
+			
+			mlModel.train(train_set);
+			train_set = null;
+			System.gc();
+			
+			List<MLExample> test_set = new ArrayList<MLExample>();
+			for(int

a066024ca2ac9456b88b56438909552a7d7d41b9
ExpressLens <expresslens@proton.me>
2023-07-12 13:20:06 +0000

    Update sphinx from 1.6.3 to 1.6.4

--- src/main/java/rainbownlp/analyzer/sentenceclause/SentenceClauseManager.java
+++ src/main/java/rainbownlp/analyzer/sentenceclause/SentenceClauseManager.java
@@ -33,4 +33,29 @@ public class SentenceClauseManager {
 	// the same as above just has all the original tokens
 	public HashMap<Integer, String> tokenMap = new HashMap<Integer, String>();
 	
-	//the m
+	//the mapping of lemmas to originals
+	//TODO: if we have repeated that are different originals it will be overwritten
+	public HashMap<String, String> lemmaTokenMap = new HashMap<String, String>();
+	
+	// this hash keep the location of each observed offset in the sentence
+	public HashMap<Integer, Clause> clauseMap = new HashMap<Integer, Clause>();
+	//////////////////////////////////////
+	//this array will keep all the lines that the governor or dependent clause could not be resolved
+	ArrayList<DependencyLine> phrases = new ArrayList<DependencyLine>();
+	public String filename;
+	public String[] normalized_dependencies;
+	public ArrayList<String> getPhrases()
+	{
+		ArrayList<String> phrase_strings = new ArrayList<String>();
+		for (DependencyLine depLine:phrases)
+		{
+			if (depLine.firstOffset<depLine.secondOffset)
+			{
+				phrase_strings.add(depLine.firstPart+" "+depLine.secondPart);
+			}
+			else
+			{
+				phrase_strings.add(depLine.secondPart+" "+depLine.firstPart);
+			}
+		}
+		return phrase_strings;

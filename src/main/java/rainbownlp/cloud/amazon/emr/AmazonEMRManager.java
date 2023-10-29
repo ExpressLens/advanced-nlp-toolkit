43ff5fe7570de2976be8d88d89cf659e5332dec2
ExpressLens <expresslens@proton.me>
2023-10-29 15:16:59 +0000

    ws

--- src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
+++ src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
@@ -5,4 +5,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.PropertiesCredenti
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsRequest;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsResult;
+import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.StepConfig;
+import com.amazonaws.services.elasticmapreduce.util.StepFactory;
+
+public class AmazonEMRManager {
+	public void runOnEMR(List<HadoopJarStepConfig> steps){
+		AWSCredentials credentials = null;
+		try {
+			credentials = new PropertiesCredentials(
+					AmazonEMRManager.class.getResourceAsStream("AwsCredentials.properties"));
+		} catch (IOException e1) {
+			System.out.println("Credentials were not properly entered into AwsCredentials.properties.");
+			System.out.println(e1.getMessage());
+			System.exit(-1);
+		}
+
+		AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(credentials);
+
+		List<StepConfig> stepsConfig = new ArrayList<StepConfig>();
+		int 

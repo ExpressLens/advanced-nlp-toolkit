bb58dbe0afeff5be30d1cde76d9521b081e7e264
ExpressLens <expresslens@proton.me>
2024-03-19 08:08:30 +0000

    updated package name

--- src/main/java/rainbownlp/core/PhraseLink.java
+++ src/main/java/rainbownlp/core/PhraseLink.java
@@ -31,4 +31,43 @@ public class PhraseLink implements Serializable  {
 	private LinkType linkTypeReal;
 	private LinkType linkTypeClosure;
 	private LinkType linkTypeIntegrated;
-	private S
+	private String altLinkID;
+	
+	
+	
+	public static PhraseLink getInstance(int pLinkID) {
+		String hql = "from PhraseLink where phraseLinkId = "+pLinkID;
+		PhraseLink phrase_link_obj = 
+			(PhraseLink)HibernateUtil.executeReader(hql).get(0);
+		return phrase_link_obj;
+	}
+
+
+	public static PhraseLink getInstance(Phrase pfromPhrase, Phrase pToPhrase){
+		String hql = "from PhraseLink where fromPhrase = "+
+			pfromPhrase.getPhraseId()+" and toPhrase="+pToPhrase.getPhraseId();
+		
+		List<PhraseLink> link_objects = 
+				(List<PhraseLink>) HibernateUtil.executeReader(hql);
+	    
+	    
+		PhraseLink phraseLink_obj;
+	    if(link_objects.size()==0)
+	    {
+	    	phraseLink_obj = createInstance(pfromPhrase, pToPhrase);
+	    	
+	    }else
+	    {
+	    	phraseLink_obj = 
+	    		link_objects.get(0);
+	    }
+	    return phraseLink_obj;
+	}
+	
+	
+	public static PhraseLink createInstance(Phrase pfromPhrase, Phrase pToPhrase){
+	    
+		PhraseLink phraseLink_obj;
+	   
+    	phraseLink_obj = new PhraseLink();
+    	phraseLink_obj.setFromPhrase(pfromPhra

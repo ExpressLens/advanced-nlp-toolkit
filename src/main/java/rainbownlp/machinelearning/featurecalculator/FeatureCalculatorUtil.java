f3cfd8c7a5bee49f92d6f5d0a177370665eba470
ExpressLens <expresslens@proton.me>
2024-07-10 18:24:04 +0000

    #628

--- src/main/java/rainbownlp/machinelearning/featurecalculator/FeatureCalculatorUtil.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/FeatureCalculatorUtil.java
@@ -10,4 +10,37 @@ public class FeatureCalculatorUtil {
 	public static List<String> getNGramBefore(Artifact startArtifact, int n) {
 		ArrayList<String> ngrams = new ArrayList<String>();
 		int stepRemained = n;
-		Artif
+		Artifact curArtifact = startArtifact.getPreviousArtifact();
+		String curInclusiveString = startArtifact.getContent();
+		while(stepRemained > 0 && curArtifact != null)
+		{
+			String curContent = curArtifact.getContent().trim();
+			if(curContent.equals(""))
+				continue;
+			
+			curInclusiveString = curContent + "_" + curInclusiveString;
+			String jumpString = curContent+"_"+startArtifact.getContent();
+			curArtifact = curArtifact.getPreviousArtifact();
+			stepRemained--;
+			
+			ngrams.add(curInclusiveString);
+			ngrams.add(jumpString);
+		}
+		
+		return ngrams;
+	}
+
+	public static List<String> getNGramAfter(Artifact endArtifact, int n) {
+		ArrayList<String> ngrams = new ArrayList<String>();
+		int stepRemained = n;
+		Artifact curArtifact = endArtifact.getNextArtifact();
+		String curInclusiveString = endArtifact.getContent();
+		while(stepRemained > 0 && curArtifact != null)
+		{
+			String curContent = curArtifact.getContent().trim();
+			if(curContent.equals(""))
+				continue;
+			
+			curInclusiveString += "_" + curContent;
+			String jumpString = endArtifact.getContent() + "_" + curContent;
+			curArtifact = curA

e546f525745071c76ed7c0bc2748d61bf36b4f16
ExpressLens <expresslens@proton.me>
2024-07-12 16:23:14 +0000

    docs WIP

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
@@ -25,4 +25,37 @@ public class LinkGeneralFeatures implements IFeatureCalculator {
 		String experimentgroup = "LinkClassificationEventEvent";
 		List<MLExample> trainExamples = 
 			MLExample.getAllExamples(experimentgroup, true);
-		int c
+		int counter = 0;
+		for (MLExample example:trainExamples)
+		{
+			LinkGeneralFeatures lbf = new LinkGeneralFeatures();
+			lbf.calculateFeatures(example);
+			counter++;
+			FileUtil.logLine(null, "Processed : "+counter +"/"+trainExamples.size());
+		}
+		
+	}
+	@Override
+	public void calculateFeatures(MLExample exampleToProcess) {
+			
+			PhraseLink phraseLink = exampleToProcess.getRelatedPhraseLink();
+			Phrase phrase1 = phraseLink.getFromPhrase();
+			Phrase phrase2 = phraseLink.getToPhrase();
+			
+			String relation_location_type = getInterMentionLocationType(phrase1,phrase2);
+			
+			
+			FeatureValuePair argumentTypeFeature = FeatureValuePair.getInstance(
+						FeatureName.InterMentionLocationType, 
+						relation_location_type,"1");
+				
+			MLExampleFeature.setFeatureExample(exampleToProcess, argumentTypeFeature);
+			
+			// The content of the args
+			FeatureValuePair fromPhraseContentFeature = FeatureValuePair.getInstance(
+					FeatureName.FromPhraseContent, 
+					phrase1.getPhraseContent(), "1");
+			
+			MLExampleFeature.setFeatureExample(exampleToProcess, fromPhraseContentFeature);
+			
+			FeatureValuePair toPhraseContentFeature = Feat

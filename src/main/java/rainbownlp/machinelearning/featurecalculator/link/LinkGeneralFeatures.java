86e0c38c0b001d24516b1b4c1d52e85af8212d5d
ExpressLens <expresslens@proton.me>
2024-07-12 13:16:14 +0000

    fix a minor bug

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
@@ -4,34 +4,25 @@
 package rainbownlp.machinelearning.featurecalculator.link;
 
 import java.util.List;
+
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.core.Phrase;
+import rainbownlp.core.PhraseLink;
+import rainbownlp.core.FeatureValuePair.FeatureName;
 import rainbownlp.machinelearning.IFeatureCalculator;
 import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
 import rainbownlp.util.FileUtil;
 
 /**
- * @author ehsan
+ * @author Azadeh
  * 
  */
-public class ConceptsDistance implements IFeatureCalculator {
+public class LinkGeneralFeatures implements IFeatureCalculator {
+	
 	public static void main (String[] args) throws Exception
 	{
 		String experimentgroup = "LinkClassificationEventEvent";
 		List<MLExample> trainExamples = 
 			MLExample.getAllExamples(experimentgroup, true);
-		int counter = 0;
-		for (MLExample example:trainExamples)
-		{
-			ConceptsDistance lbf = new ConceptsDistance();
-			lbf.calculateFeatures(example);
-			counter++;
-			FileUtil.logLine(null, "Processed : "+counter +"/"+trainExamples.size());
-		}
-		
-	}
-	@Override
-	public void calculateFeatures(MLExample exampleToProcess) {
-		
-	
-	}
-	
-}
+		int c

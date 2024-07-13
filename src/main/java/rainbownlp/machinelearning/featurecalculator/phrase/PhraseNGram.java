31b9caa1b03acb7fd3f5cd7cb5a19b7adc4a0e0b
ExpressLens <expresslens@proton.me>
2024-07-13 14:58:36 +0000

    ignore binary

--- src/main/java/rainbownlp/machinelearning/featurecalculator/phrase/PhraseNGram.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/phrase/PhraseNGram.java
@@ -27,4 +27,38 @@ public class PhraseNGram implements IFeatureCalculator {
 		calculateInPhraseNGram(2, p, exampleToProcess, "InPhrase2Gram");
 		
 		List<String> beforePhraseNGrams = FeatureCalculatorUtil.getNGramBefore(p.getStartArtifact(), 3);
-		List<String> afterPhraseNGrams = FeatureCalculatorUtil.getNGramAfter(p.getE
+		List<String> afterPhraseNGrams = FeatureCalculatorUtil.getNGramAfter(p.getEndArtifact(), 3);
+		
+		for(String beforeNGram : beforePhraseNGrams)
+		{
+			FeatureValuePair value_pair = FeatureValuePair.getInstance(
+					"BeforePhraseNGram", beforeNGram, "1");
+			MLExampleFeature.setFeatureExample(exampleToProcess,value_pair);
+		}
+		for(String afterNGram : afterPhraseNGrams)
+		{
+			FeatureValuePair value_pair = FeatureValuePair.getInstance(
+					"AfterPhraseNGram", afterNGram, "1");
+			MLExampleFeature.setFeatureExample(exampleToProcess,value_pair);
+		}
+	}
+	
+	void calculateInPhraseNGram(int n, Phrase phrase, MLExample example, String featureName)
+	{
+		String[] word_text = 
+				StringUtil.getTermByTermWordnet(phrase.getPhraseContent().toLowerCase()).split(" ");
+			
+		for(int i=0;i<word_text.length-n;i++)
+		{
+			String cur_content = "";
+			for(int j=0;j<n;j++)
+			{
+				int new_part_index = i+j;
+				if(!word_text[new_part_index].trim().equals(""))
+				{
+					cur_content = 
+						cur_content.concat("_"+word_text[new_part_index].trim());
+				}
+			}
+			cur_content = cur_content.replaceAll("^_", "");
+			FeatureValuePair value_pair = FeatureValuePair.getIns

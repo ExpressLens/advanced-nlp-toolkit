91d8060543031d1629f05ab7984c1d72dc653fa6
ExpressLens <expresslens@proton.me>
2024-07-18 17:45:15 +0000

    Cleanup files

--- src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceNGram.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceNGram.java
@@ -19,4 +19,36 @@ public class SentenceNGram implements IFeatureCalculator {
 			Artifact firstSentence = sentencesLink.getFirstPhrase().getStartArtifact();
 			Artifact secondSentence = sentencesLink.getSecondPhrase().getStartArtifact();
 			calculateSentenceNGram(1, firstSentence, exampleToProcess, "FirstSentence1Gram");
-			calculateSentenceNGram(1, secondSentence, ex
+			calculateSentenceNGram(1, secondSentence, exampleToProcess, "SecondSentence1Gram");
+			calculateSentenceNGram(2, firstSentence, exampleToProcess, "FirstSentence2Gram");
+			calculateSentenceNGram(2, secondSentence, exampleToProcess, "SecondSentence2Gram");
+		}else
+		{
+			Artifact sentence = exampleToProcess.getRelatedPhrase().getStartArtifact();
+			if(sentence.getArtifactType() ==  Type.Sentence)
+			{
+				calculateSentenceNGram(1, sentence, exampleToProcess, "Sentence1Gram");
+				calculateSentenceNGram(2, sentence, exampleToProcess, "Sentence2Gram");
+			}
+		}
+	}
+	
+	void calculateSentenceNGram(int n, Artifact sentence, MLExample example, String featureName)
+	{
+		String[] word_text = 
+				StringUtil.getTermByTermWordnet(sentence.getContent().toLowerCase()).split(" ");
+			
+		for(int i=0;i<word_text.length-n;i++)
+		{
+			String cur_content = "";
+			for(int j=0;j<n;j++)
+			{
+				int new_part_index = i+j;
+				if(!word_text[new_part_index].trim().equals(""))
+				{
+					cur_content = 
+						cur_content.concat("_"+word_text[new_part_index].trim());
+				}
+			}
+			cur_content = cur_content.replaceAll("^_", "");
+			FeatureVa

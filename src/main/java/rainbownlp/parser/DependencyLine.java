ebf0d7efd4c770d973a6a9dcfb081a9beaccf181
ExpressLens <expresslens@proton.me>
2024-07-21 15:32:08 +0000

    Update generated README

--- src/main/java/rainbownlp/parser/DependencyLine.java
+++ src/main/java/rainbownlp/parser/DependencyLine.java
@@ -4,4 +4,12 @@ public class DependencyLine {
 	public String relationName;
 	public String firstPart;
 	public String secondPart;
-	public
+	public int firstOffset;
+	public int secondOffset;
+	public boolean hasWord(String word) {
+		if(firstPart.equals(word) ||
+				secondPart.equals(word))
+			return true;
+		return false;
+	}
+}
---
+++ src/main/java/rainbownlp/parser/ParseHandler.java
@@ -0,0 +1,23 @@
+package rainbownlp.parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import opennlp.tools.chunker.ChunkerME;
+import opennlp.tools.chunker.ChunkerModel;
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+import

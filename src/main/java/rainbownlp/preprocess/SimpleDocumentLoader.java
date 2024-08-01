04522713107420521d3fcd7931fb23d2abcbd186
ExpressLens <expresslens@proton.me>
2024-08-01 19:38:18 +0000

    updated jar

--- src/main/java/rainbownlp/preprocess/SimpleDocumentLoader.java
+++ src/main/java/rainbownlp/preprocess/SimpleDocumentLoader.java
@@ -18,4 +18,32 @@ public class SimpleDocumentLoader extends DocumentAnalyzer {
 		return documents;
 	}
 	/**
-	 * Simply call loadSentences, override this method for 
+	 * Simply call loadSentences, override this method for more complex loaders
+	 * @param doc
+	 * @throws IOException
+	 * @throws Exception 
+	 */
+	protected void processDocument(Artifact doc) throws Exception{
+		loadSentences(doc);
+	}
+
+	/**
+	 * Use tokenizer to find sentences
+	 * @param parentArtifact can be document, paragraph or any other document section
+	 * @throws IOException
+	 */
+	protected void loadSentences(Artifact parentArtifact) throws IOException {
+		Tokenizer docTokenizer = new Tokenizer(parentArtifact.getAssociatedFilePath());
+		HashMap<Integer, String> setences = docTokenizer.getSentences();
+		List<Artifact> setencesArtifacts = new ArrayList<Artifact>();
+		Artifact previous_sentence = null;
+
+		for(int curSentenceIndex=0;
+				curSentenceIndex<setences.keySet().size();curSentenceIndex++){
+			//			System.out.print("\r Loading sentences for: "+parentDoc.get_associatedFilePath()+ " "+
+			//					curSentenceIndex + "/ " + setences.size()+longspace);
+
+			String tokenizedSentence = setences.get(curSentenceIndex);
+			List<Integer> tokens_starts = 
+					docTokenizer.sentences_tokens_indexes.get(curSentenceIndex);
+			List<String>

5aadc9765e77d84d239e5ae53146c5bfa14aa6a5
ExpressLens <expresslens@proton.me>
2024-08-02 09:04:41 +0000

    Update extensions.

--- src/main/java/rainbownlp/preprocess/SimpleDocumentLoader.java
+++ src/main/java/rainbownlp/preprocess/SimpleDocumentLoader.java
@@ -46,4 +46,34 @@ public class SimpleDocumentLoader extends DocumentAnalyzer {
 			String tokenizedSentence = setences.get(curSentenceIndex);
 			List<Integer> tokens_starts = 
 					docTokenizer.sentences_tokens_indexes.get(curSentenceIndex);
-			List<String>
+			List<String> tokens = 
+					docTokenizer.sentences_tokens_string.get(curSentenceIndex);
+
+			if(tokens.size()==0)
+				continue;
+
+			Artifact new_sentence = Artifact.getInstance(Artifact.Type.Sentence,
+					parentArtifact.getAssociatedFilePath(), tokens_starts.get(0));//line number start from 1
+			new_sentence.setParentArtifact(parentArtifact);
+			new_sentence.setLineIndex(curSentenceIndex+1);
+			new_sentence.setContent(tokenizedSentence);
+			new_sentence.setArtifactOptionalCategory(dsType.name());
+			if (previous_sentence != null) {
+				new_sentence.setPreviousArtifact(previous_sentence);
+				previous_sentence.setNextArtifact(new_sentence);
+				HibernateUtil.save(previous_sentence);
+			}
+
+			HibernateUtil.save(new_sentence);
+
+			loadWords(new_sentence,tokens, tokens_starts, curSentenceIndex);
+
+			setencesArtifacts.add(new_sentence);
+
+			//Pattern.insert(Util.getSentencePOSsPattern(curSentence));
+			previous_sentence = new_sentence;
+			HibernateUtil.clearLoaderSession();
+		}
+	}
+
+	protected void loadWords(Artifact parentSentence,  List<Stri

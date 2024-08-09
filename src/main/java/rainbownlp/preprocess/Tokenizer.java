fa3b0b21a82bea90fe7e7e74d493ee458b910646
ExpressLens <expresslens@proton.me>
2024-08-09 10:12:57 +0000

    Update core-java.md

--- src/main/java/rainbownlp/preprocess/Tokenizer.java
+++ src/main/java/rainbownlp/preprocess/Tokenizer.java
@@ -28,4 +28,30 @@ public class Tokenizer {
 	public static List<Word> getTokens(String sentence)
 	{
 		if(tf == null)
-			tf = 
+			tf = PTBTokenizer.factory();
+		
+		List<Word> tokens_words = tf.getTokenizer(new StringReader(sentence)).tokenize();
+		
+		
+		return tokens_words;
+	}
+	public static void fixDashSplitted() throws SQLException
+	{
+//		String q = "SELECT * from Artifact where text_content like '%-%' and artifact_type = 'Word'";
+//		ResultSet artifacts = Util.db.executeReader(q);
+//		while(artifacts.next())
+//		{
+//			int artifact_id = artifacts.getInt("artifact_id");
+//			Artifact curArti = new Artifact(artifact_id);
+//			fixMergedNameEntity(curArti);
+//		}
+	}
+	public ArrayList<String> paragraphs = new ArrayList<String>();
+	public HashMap<Integer, String> sentences = new HashMap<Integer, String>();
+	public HashMap<Integer,List<Integer>> sentences_tokens_indexes = new HashMap<Integer, List<Integer>>();
+	public HashMap<Integer,List<Word>> sentences_tokens = new HashMap<Integer,List<Word>>();
+	public Hashtable<Integer, List<String>> sentences_tokens_string = new Hashtable<Integer, List<String>>();
+	
+	public static void fixMergedNameEntity(Artifact curArtifact) throws SQLException
+	{
+//		St
